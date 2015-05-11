d10e9bf779f40eceda6612838393dc0f7d02aa40
jiangdou <jiangdou8888@gmail.com>
2015-05-11 11:05:44 +0800

    Update rk3288_recovery_不能reboot.c

--- doc/rk3288_recovery_不能reboot.c
+++ doc/rk3288_recovery_不能reboot.c
@@ -12,3 +12,24 @@
 android_reboot（）  -->>
 
 /system/core/libcutils/android_reboot.c:int android_reboot(int cmd, int flags, char *arg)
+
+
+//system/core/include/cutils/android_reboot.h
+
+
+
+///add  Anroid.mk   at  /system/core/libcutils/
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcutils
+LOCAL_SRC_FILES := $(commonSources) \
+        android_reboot.c \
+        ashmem-dev.c \
+        debugger.c \
+        klog.c \
+        partition_utils.c \
+        properties.c \
+        qtaguid.c \
+        trace.c \
+        uevent.c \
+		jiangdou_reboot.c
